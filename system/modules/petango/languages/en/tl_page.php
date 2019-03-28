39d35809dd005c1bc85338954d47ad9c7d1516fa
asconsulting <andrew@andrewstevens.consulting>
2019-03-28 13:03:48 -0400

    Update tl_page.php

--- system/modules/petango/languages/en/tl_page.php
+++ system/modules/petango/languages/en/tl_page.php
@@ -14,11 +14,11 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_petango_site']['petango_animal_reader'] = array('Animal Reader', 'This page is used as an animal reader');
-$GLOBALS['TL_LANG']['tl_petango_site']['petango_site_reader'] 	= array('Site Reader', 'This page is used as an site reader');
+$GLOBALS['TL_LANG']['tl_page']['petango_animal_reader'] 	= array('Animal Reader', 'This page is used as an animal reader');
+$GLOBALS['TL_LANG']['tl_page']['petango_site_reader'] 		= array('Site Reader', 'This page is used as an site reader');
 
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_petango_site']['petango_legend'] 		= 'Petango details';
+$GLOBALS['TL_LANG']['tl_page']['petango_legend'] 			= 'Petango details';
